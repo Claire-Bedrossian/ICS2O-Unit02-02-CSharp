f25ff198d3566bc606210fdc139d69f6a96e87c7
Claire-Bedrossian <113055648+Claire-Bedrossian@users.noreply.github.com>
2022-10-11 14:16:57 -0400

    Added function

---
+++ main.cs
@@ -0,0 +1,18 @@
+// Created by: Claire Bedrossian
+// Created on: Oct 2022
+//
+// This program calculates the perimeter and area of a rectangle
+
+using System; 
+
+public class Program 
+{
+
+    public static void Main(string[] args) 
+    {
+        Console.WriteLine("Perimeter of a 12 x 7 rectangle: " + (7 * 2 + 12 *2));
+        Console.WriteLine("Area of a 12 x 7 rectangle: " + (7 * 12));
+
+        Console.WriteLine("\nDone.");
+    }
+}
