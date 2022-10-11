fca1731faef83c508faa72da5b8710c389ab88e7
Claire-Bedrossian <113055648+Claire-Bedrossian@users.noreply.github.com>
2022-10-11 14:32:13 -0400

    Fixed whitespace formating

--- main.cs
+++ main.cs
@@ -3,14 +3,14 @@
 //
 // This program calculates the perimeter and area of a rectangle
 
-using System; 
+using System;
 
-public class Program 
+public class Program
 {
 
-    public static void Main(string[] args) 
+    public static void Main(string[] args)
     {
-        Console.WriteLine("Perimeter of a 12 x 7 rectangle: " + (7 * 2 + 12 *2));
+        Console.WriteLine("Perimeter of a 12 x 7 rectangle: " + (7 * 2 + 12 * 2));
         Console.WriteLine("Area of a 12 x 7 rectangle: " + (7 * 12));
 
         Console.WriteLine("\nDone.");
